74cc24454d9936d82872513ddec58a40e290bf49
urayfajri <uraywidiansyah88@gmail.com>
2022-07-25 12:34:29 +0700

    Initial Commit

---
+++ NC2-DaiRefl/NC2_DaiReflApp.swift
@@ -0,0 +1,17 @@
+//
+//  NC2_DaiReflApp.swift
+//  NC2-DaiRefl
+//
+//  Created by Uray Muhamad Noor Fajri Widiansyah on 25/07/22.
+//
+
+import SwiftUI
+
+@main
+struct NC2_DaiReflApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
